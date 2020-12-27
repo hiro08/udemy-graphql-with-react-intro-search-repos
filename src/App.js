4e2bc65ae0771c9df9e0683fc179c9a0e5c5be58
Hiroaki Kawana <hiroakikawana@gmail.com>
2020-12-27 03:04:05 +0000

    setup graphql client

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
+import { ApolloProvider } from 'react-apollo'
+import gql from 'graphql-tag'
+import { Query } from 'react-apollo'
+import client from './client'
+
+const ME = gql`
+  query me {
+    user(login: "iteachonudemy") {
+      name
+      avatarUrl
+    }
+  }
+`
+
 function App() {
   return (
-    <div>Hello, GraphQL</div>
+    <ApolloProvider client={client}>
+      <div>Hello, GraphQL</div>
+      <Query query={ME}>
+        {
+          ({ loading, error, data }) => {
+            if (loading) return 'Loading...'
+            if (error) return `Error: ${error.message}`
+
+            return <div>{data.user.name}</div>
+          }
+        }
+      </Query>
+    </ApolloProvider>
   )
 }
 
